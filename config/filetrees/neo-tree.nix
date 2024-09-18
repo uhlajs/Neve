ced6032f75943f53678ce86b29d4d4ed8b913bc8
Jan Uhlík <jan@uhlik.me>
2024-09-18 08:50:42 +0200

    Fix neo tree followCurrentFile

--- config/filetrees/neo-tree.nix
+++ config/filetrees/neo-tree.nix
@@ -9,11 +9,12 @@
     popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
     buffers = {
       bindToCwd = false;
-      followCurrentFile = {
-        enabled = true;
-      };
     };
     filesystem = {
+      followCurrentFile = {
+        enabled = true;
+        leaveDirsOpen = false;
+      };
       filteredItems = {
         visible = true;
         showHiddenCount = false;
