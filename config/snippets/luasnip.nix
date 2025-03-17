fa156e92864215aacd15faabe02066cc68d6b6a0
Jan Uhlík <jan@uhlik.me>
2025-03-17 09:44:30 +0100

    Configure snippets

--- config/snippets/luasnip.nix
+++ config/snippets/luasnip.nix
@@ -12,8 +12,8 @@
     plugins.luasnip = {
       enable = true;
       settings = {
-        enable_autosnippets = true;
-        store_selection_keys = "<Tab>";
+        enable_autosnippets = false;
+        store_selection_keys = "<C-y>";
       };
       fromVscode = [
         {
