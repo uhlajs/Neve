dd2c340fbb7dda7a824544e1384f67ec8ef490f9
Jan Uhlík <jan@uhlik.me>
2025-09-12 19:10:57 +0200

    Enable dap

--- config/default.nix
+++ config/default.nix
@@ -24,7 +24,7 @@
   bufferlines.enable = lib.mkDefault true;
   colorschemes.enable = lib.mkDefault true;
   completion.enable = lib.mkDefault true;
-  dap.enable = lib.mkDefault false;
+  dap.enable = lib.mkDefault true;
   filetrees.enable = lib.mkDefault true;
   git.enable = lib.mkDefault true;
   keys.enable = true;
